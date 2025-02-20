2c2d6077dba3ea07ef590c2740cf0d5c93cf1645
ananyaravikumar548 <ananyaravikumar71@gmail.com>
2025-02-20 12:17:58 +0530

    Update program1.py

--- lab2/program1.py
+++ lab2/program1.py
@@ -1,1 +1,12 @@
+def fibonacci(n): 
+ if n == 0: 
+ return 0 
+ elif n == 1: 
+ return 1 
+ else: 
+ return fibonacci(n - 1) + fibonacci(n - 2) 
+# Example usage: 
+number = 6 
+result = fibonacci(number) 
+print(f"The {number}th Fibonacci number is {result}")
 
